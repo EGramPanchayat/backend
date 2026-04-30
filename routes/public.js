15a83b5d2e7995934f3cc9c7fe13cddb2d5bff11
EGramPanchayat <anwit3171@gmail.com>
2026-04-30 20:59:04 +0530

    Fixed errors of wronge routing

--- routes/public.js
+++ routes/public.js
@@ -5,7 +5,7 @@ import { getQR } from "../controllers/qr.controller.js";
 import { getPaymentQR } from "../controllers/dakhalaMagani.controller.js";
 import { getNews } from "../controllers/news.controller.js";
 import { getNotices } from "../controllers/notices.controller.js";
-import {  getExecutiveBoard } from "../controllers/executiveBoard.controller.js";
+import { changeExecutiveBoard, getExecutiveBoard } from "../controllers/executiveBoard.controller.js";
 import { getDevWorks} from "../controllers/developementWorks.controller.js";
 
 
@@ -15,17 +15,12 @@ const router = Router();
 router.post("/certificate-request", imageUpload.single("file"), createDakhala);
 
 
-router.route("/devworks")
-      .get(getDevWorks)
+router.get("/devworks", getDevWorks);
 
-
-router.get("/news", getNews);
-
-
-router.get("/notices", getNotices);
 
 // Public content endpoints
-
+router.get("/news", getNews);
+router.get("/notices", getNotices);
 
 // Payment qr and general qr
 router.get("/qr", getQR);
@@ -33,7 +28,12 @@ router.get("/payment-qr", getPaymentQR);
 
 
 // Executive board
-router.get("/executive-board", getExecutiveBoard)
+router.route("/executive-board")
+  .get(getExecutiveBoard)
+  .post(
+    imageUpload.any(),
+    changeExecutiveBoard
+  );
 
 
 export default router;
